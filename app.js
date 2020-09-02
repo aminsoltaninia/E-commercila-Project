ae90a79089aa7910ad74221ba075e65de8a28754
aminsoltaninia <aminsoltani.nia@gmail.com>
2020-09-02 23:22:22 +0430

    auth an add mongodb

--- app.js
+++ app.js
@@ -3,13 +3,29 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const crypto = require('crypto');
+const util = require('util');
+const scrypt = util.promisify(crypto.scrypt) //  change promis calback to awat and async function
+
+const User = require('./models/user.js')
 
 
-app.use(bodyParser.urlencoded({extended:true}) )
+// set config mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/ecomerce', { useNewUrlParser: true })
 
 
+// config express
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieSession({
+    keys: ['#$jhsgd58;kl8bbh^Gfgdgdemf_)N']
+}))
 
-app.get('/', (req, res) => {
+// routers
+
+app.get('/signup', async (req, res) => {
 
     res.send(`
      <div>
@@ -34,9 +50,57 @@ app.get('/', (req, res) => {
 //  This object will contain key-value pairs, where the value can be a string or array (when extended is false), 
 //  or any type (when extended is true).
 
-app.post('/' ,(req,res)=>{
-    console.log('req.body : ' , req.body)
+app.post('/signup', async (req, res) => {
+    // console.log('req.body : ' , req.body)
+    let { email, password, passwordConf } = req.body;
+    let user = await User.findOne({ email })
+    if (user) return res.send('user is exist , plese inter another email')
+
+    if (password !== passwordConf) return res.send('password and confPassword arnt equal ')
+    let salt = crypto.randomBytes(8).toString('hex')
+
+    const hashPass = await scrypt(password, salt, 64)
+    const newUser = new User({
+        email,
+        password: hashPass.toString('hex') + `.${salt}`
+    })
+    req.session.userId = newUser._id;
+
+    await newUser.save((err) => {
+        if (err) throw err;
+    });
     res.send('singn up seccess')
+})
+
+app.get('/signout', (req, res) => {
+    req.session = null;
+    res.send('you are log out ')
+})
+
+app.get('/signin', (req, res) => {
+    res.send(`  
+    <div>
+    <form action="" method="POST" >
+     <input name ="email" type="email" placeholder="email : ">
+     <input  name="password" type="password" placeholder="password : ">
+     <button type="submit">submit</button>
+    </form>
+   </div>
+    `)
+})
+
+app.post('/signin', async (req, res) => {
+    let { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.send('user not found')
+    const [oldPass, salt] = user.password.split('.')
+
+    const supliedPass = await scrypt(password, salt, 64);
+
+    console.log(oldPass, supliedPass, salt)
+
+    if (supliedPass.toString('hex') !== oldPass) return res.send('password is incorect')
+    res.send('you are login')
 })
 
 app.listen(3000, () => {
